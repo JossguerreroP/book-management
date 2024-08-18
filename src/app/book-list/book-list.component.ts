1bb7d9980a2758e8bfab0399bc508b9ac0ccbd1d
JossguerreroP <josguerrerop@udistrital.edu.co>
2024-08-17 23:26:20 -0500

    Melo

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Book } from '../models/book';
+import { AddBook,RemoveBook } from '../books/book.actions';
+
 
 @Component({
   selector: 'app-book-list',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
+
+constructor(private store: Store<{books:Book[]}>){
+
+}
+
+addBook(id:string,title:string, author:string){
+  this.store.dispatch(AddBook({id,title,author}));
+}
+
+removeBook(bookId:string){
+  this.store.dispatch(RemoveBook({bookId}));
+}
 
 }
